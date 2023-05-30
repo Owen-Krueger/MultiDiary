6e7176caeb834e673aa28f8609520bbeaefea829
Owen Krueger <github@owenkrueger.dev>
2023-05-30 16:39:03 -0500

    Fix building and add android back in

--- src/MauiProgram.cs
+++ src/MauiProgram.cs
@@ -11,37 +11,37 @@ namespace MultiDiary;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.UseMauiCommunityToolkit()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
 
-		builder.Services.AddMauiBlazorWebView();
+        builder.Services.AddMauiBlazorWebView();
 
 #if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
-		builder.Logging.AddDebug();
+        builder.Services.AddBlazorWebViewDeveloperTools();
+        builder.Logging.AddDebug();
 #endif
 
         builder.Services.AddMudServices(config =>
-		{
-			config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
-		});
+        {
+            config.SnackbarConfiguration.PositionClass = Defaults.Classes.Position.BottomRight;
+        });
         builder.Services.AddScoped<System.IO.Abstractions.IFileSystem, System.IO.Abstractions.FileSystem>();
         builder.Services.AddSingleton(FolderPicker.Default);
         builder.Services.AddSingleton<StateContainer>();
-		builder.Services.AddSingleton(Preferences.Default);
-		builder.Services.AddSingleton(SecureStorage.Default);
-		builder.Services.AddSingleton<IWebDavClient, WebDavClient>();
-		builder.Services.AddTransient<IWebDavService, WebDavService>();
-		builder.Services.AddTransient<IDiaryService, DiaryService>();
+        builder.Services.AddSingleton(Preferences.Default);
+        builder.Services.AddSingleton(SecureStorage.Default);
+        builder.Services.AddSingleton<IWebDavClient, WebDavClient>();
+        builder.Services.AddTransient<IWebDavService, WebDavService>();
+        builder.Services.AddTransient<IDiaryService, DiaryService>();
 
-		return builder.Build();
-	}
+        return builder.Build();
+    }
 }
